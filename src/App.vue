30f6da30ca6daf9390d4a56115c3b18c965a7e55
Diala Egwuchukwu Stephen <46764458+egdiala@users.noreply.github.com>
2020-06-09 00:29:44 +0100

    Designed the home page

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,100 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar absolute color="primary" elevate-on-scroll v-resize="method1">
+      <v-toolbar-title class="font-weight-medium">
+        WallcoverHanger
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
         text
+        class="d-none d-xl-flex d-lg-flex d-xl-none d-md-none d-lg-flex d-sm-none d-md-flex d-sm-flex"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Portfolio</span>
       </v-btn>
+
+      <v-switch
+        class="d-none d-xl-flex d-lg-flex d-xl-none d-md-none d-lg-flex d-sm-none d-md-flex d-sm-flex"
+        v-model="$vuetify.theme.dark"
+        hide-details
+        :label="`${switch1}`"
+        @change="changeState"
+      ></v-switch>
+
+      <v-menu bottom left v-if="menu == true">
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on" class="">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-btn text>
+              <span class="mr-2">Portfolio</span>
+            </v-btn>
+          </v-list-item>
+
+          <v-list-item>
+            <v-switch
+              v-model="$vuetify.theme.dark"
+              hide-details
+              :label="`${switch1}`"
+              @change="changeState"
+            ></v-switch>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <router-view></router-view>
     </v-content>
+
+    <v-footer class="pa-0">
+      <v-card flat tile width="100%" color="primary">
+        <v-card-text class="text-center">
+          © <strong>WallcoverHanger</strong> {{ new Date().getFullYear() }}
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld
-  },
+  components: {},
 
   data: () => ({
-    //
-  })
+    switch1: "Light",
+    menu: true,
+    m: "dd",
+  }),
+
+  mounted: function() {
+    this.method1(); //method1 will execute at pageload
+  },
+
+  methods: {
+    //changes theme manually
+    changeState() {
+      if (this.switch1 == "Light" && this.$vuetify.theme.dark == true) {
+        this.switch1 = "Dark";
+      } else {
+        this.switch1 = "Light";
+      }
+    },
+
+    method1: function() {
+      var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+      if (isMobile) {
+        this.menu = true;
+      } else {
+        this.menu = false;
+      }
+    },
+  },
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,25 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#ffffff",
+                secondary: "#1e1e1e",
+                accent: "#8c9eff",
+                error: "#b71c1c",
+                bground: "#ffffff",
+                btnprim: "#3F51B5",
+            },
+            dark: {
+                primary: "#1e1e1e",
+                secondary: "#ffffff",
+                accent: "#8c9eff",
+                error: "#b71c1c",
+                bground: "#e3e3e3",
+                btnprim: "#7986CB",
+            },
+        },
+    },
+});
